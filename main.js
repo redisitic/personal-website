1a4b92f42b84ecdd5fc4777ba8df940f509e6d5e
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-18 23:57:08 +0530

    T_T

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var currentYear = new Date().getFullYear();
-var yearElements = document.getElementsByClassName("year");
-for (var i = 0; i < yearElements.length; i++) {
-    yearElements[i].innerHTML = currentYear;
-}
+document.addEventListener("DOMContentLoaded", function() {
+  var currentYear = new Date().getFullYear();
+  var yearElements = document.getElementsByClassName("year");
+  for (var i = 0; i < yearElements.length; i++) {
+      yearElements[i].innerHTML = currentYear;
+  }
+});
