9a00d39a9a3cda48bc41b41c34774b82cf7a14b6
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-23 12:45:09 +0530

    Added Neomorph Branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        nm: '20px 20px 35px #999999, -20px -20px 35px #ffffff',
+        nmb: '12px 12px 30px #afafaf, -12px -12px 30px #ffffff',
+        nmd: '20px 20px 35px #0a0a0a, -20px -20px 35px #1e1e1e',
+        nmdb: '12px 12px 30px #0a0a0a, -12px -12px 30px #1e1e1e'
+      },
+      backgroundImage: {
+        dk: "linear-gradient(145deg, #121212, #151515)"
+      }
+    },
   },
   plugins: [],
 }
