b9278a426238d7f850b12d67662e2091b95bc7bc
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-24 23:52:07 +0530

    Attempt 2 at doing my job

--- main.js
+++ main.js
@@ -1,21 +1,3 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { logEvent } from "firebase/analytics";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyC_VKpzHcTwxXzakb04PxkHs56S44C2bTM",
-  authDomain: "redisitic.firebaseapp.com",
-  databaseURL: "redisitic.firebaseio.com",
-  projectId: "redisitic",
-  storageBucket: "redisitic.appspot.com",
-  messagingSenderId: "278302736666",
-  appId: "1:278302736666:web:dc4774de7936eaca9daca3",
-  measurementId: "G-6K76MZ9KF8"
-};
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-logEvent(analytics, 'notification_received');
-
 document.addEventListener("DOMContentLoaded", function() {
   var currentYear = new Date().getFullYear();
   var yearElements = document.getElementsByClassName("year");
@@ -23,3 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
       yearElements[i].innerHTML = currentYear;
   }
 });
+
+import { trackPageView } from '/analytics.js';
+trackPageView('/index.html');
