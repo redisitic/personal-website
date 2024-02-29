8161b9de21f1038ffba5ac4bfaf2eb5e1b077664
Rhythm7845~ <66195447+redisitic@users.noreply.github.com>
2024-02-29 19:43:05 +0530

    Edge rounding is visible on large screens and images dont cast shadows on each other in light mode as much

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ export default {
       boxShadow: {
         nm: '20px 20px 35px #999999, -20px -20px 35px #ffffff',
         nmb: '12px 12px 30px #afafaf, -12px -12px 30px #ffffff',
+        nma: '12px 12px 30px #afafaf',
         nmd: '20px 20px 35px #0a0a0a, -20px -20px 35px #1e1e1e',
         nmdb: '12px 12px 30px #0a0a0a, -12px -12px 30px #1e1e1e'
       },
